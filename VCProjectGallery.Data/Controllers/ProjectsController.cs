5f3543f230dcdbf40c852882580522425c883919
Ahmed Abd Elbary <AhmedAElbary@gmail.com>
2022-07-11 16:46:34 +0200

    Add PublicBaseClass and MediaBaseClass Add Project and Project Image Models Add EF-Setup Add Database Initializers Add DB Migration Add ApiBaseController Add Project Controller

---
+++ VCProjectGallery.Data/Controllers/ProjectsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VCProjectGallery.Data.Base;
+using VCProjectGallery.Models;
+using VCProjectGallery.Models.Project;
+using Microsoft.EntityFrameworkCore;
+using VCProjectGallery.Models.Base;
+
+namespace VCProjectGallery.Data.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProjectsController : ApiBaseController<Project>
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ProjectsController(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+    }
+}
---
+++ VCProjectGallery.Data/Migrations/20220711144016_init.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VCProjectGallery.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FinishedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Client = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Budget = table.Column<long>(type: "bigint", nullable: true),
+                    BudgetCurrency = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedByName = table.Column<string>(type: "nvarchar(127)", maxLength: 127, nullable: false),
+                    CreatedByID = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "DateTime2", nullable: false),
+                    UpdatedByName = table.Column<string>(type: "nvarchar(127)", maxLength: 127, nullable: true),
+                    UpdatedByID = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "DateTime2", nullable: true),
+                    DeletedByName = table.Column<string>(type: "nvarchar(127)", maxLength: 127, nullable: true),
+                    DeletedByID = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletedOn = table.Column<DateTime>(type: "DateTime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectImages",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProjectID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedByName = table.Column<string>(type: "nvarchar(127)", maxLength: 127, nullable: false),
+                    CreatedByID = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "DateTime2", nullable: false),
+                    UpdatedByName = table.Column<string>(type: "nvarchar(127)", maxLength: 127, nullable: true),
+                    UpdatedByID = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "DateTime2", nullable: true),
+                    DeletedByName = table.Column<string>(type: "nvarchar(127)", maxLength: 127, nullable: true),
+                    DeletedByID = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletedOn = table.Column<DateTime>(type: "DateTime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Extension = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Revision = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FileSize = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectImages", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ProjectImages_Projects_ProjectID",
+                        column: x => x.ProjectID,
+                        principalTable: "Projects",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectImages_ProjectID",
+                table: "ProjectImages",
+                column: "ProjectID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProjectImages");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
