48c711565d0cc3de51188e840a6656075cf3c96a
Ahmed Abd Elbary <ahmedaelbary@gmail.com>
2022-07-11 18:59:35 +0000

    Merged in feature/EF-Setup (pull request #1)
    Basic Backend Setup
    
    * Add PublicBaseClass and MediaBaseClass
    Add Project and Project Image Models
    Add EF-Setup
    Add Database Initializers
    Add DB Migration
    Add ApiBaseController
    Add Project Controller
    
    * Update Connection String for DB
    
    
    Approved-by: Wael Yassir

---
+++ VCProjectGallery.Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VCProjectGallery.Models.Project;
+
+namespace VCProjectGallery.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Project> Projects { get; set; }
+		public DbSet<ProjectImage> ProjectImages { get; set; }
+	}
+}
---
+++ VCProjectGallery.Models/Base/MediaBaseClass.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VCProjectGallery.Models.Base
+{
+	public abstract class MediaBaseClass : PublicBaseClass
+	{
+		protected MediaBaseClass() : base()
+		{ }
+
+		public virtual string FileName { get; set; }
+
+		public virtual string Extension { get; set; }
+
+		public virtual string Revision { get; set; }
+
+		public virtual string Path { get; set; }
+
+		public virtual long FileSize { get; set; }
+
+		[NotMapped]
+		public virtual byte[] FileBytes { get; set; }
+
+		[NotMapped]
+		public virtual IFormFile FormFile { get; set; }
+	}
+}
